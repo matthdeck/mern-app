3cd4da630f2451c6135e19d8af8c1345b7e7a32a
decktech <mdecker113@gmail.com>
2022-12-13 14:33:17 -0500

    REST API configured

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,66 @@
+const Goal = require('../model/goalModel')
+
+// @desc  Get goals
+// @route GET /api/goals
+// @access Private
+const getGoals = async (req, res) => {
+  const goals = await Goal.find()
+
+  res.status(200).json(goals)
+}
+
+// @desc  Set goal
+// @route POST /api/goals
+// @access Private
+const setGoal = async (req, res) => {
+  if (req.body.text == null) {
+    res.status(400)
+    throw new Error('Please add a text field')
+  }
+
+  const goal = await Goal.create({
+    text: req.body.text,
+  })
+
+  res.status(200).json(goal)
+}
+
+// @desc  Update goal
+// @route PUT /api/goals/:id
+// @access Private
+const updateGoal = async (req, res) => {
+  const goal = await Goal.findById(req.params.id)
+
+  if (goal == null) {
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true
+  })
+  res.status(200).json(updatedGoal)
+}
+
+// @desc  Delete goal
+// @route DELETE /api/goals/:id
+// @access Private
+const deleteGoal = async (req, res) => {
+  const goal = await Goal.findById(req.params.id)
+
+  if (goal == null) {
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+
+  await goal.remove()
+
+  res.status(200).json({ id: req.params.id })
+}
+
+module.exports = { 
+  getGoals, 
+  setGoal, 
+  updateGoal, 
+  deleteGoal 
+} 
