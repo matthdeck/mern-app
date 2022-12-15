eb25d46ea136fdf58e5b2d33db0faf821605ab07
decktech <mdecker113@gmail.com>
2022-12-15 15:28:38 -0500

    CRUD with auth

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -6,13 +6,9 @@ const {
   updateGoal, 
   deleteGoal 
 } = require('../controllers/goalController')
+const { protect } = require('../middleware/authMiddleware')
 
-router.route('/').get(getGoals).post(setGoal)
-// router.get('/', getGoals)
-// router.post('/', setGoal)
-
-router.route('/:id').put(updateGoal).delete(deleteGoal)
-// router.put('/:id', updateGoal)
-// router.delete('/:id', deleteGoal)
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+router.route('/:id').put(protect, updateGoal).delete(protect, deleteGoal)
 
 module.exports = router
