b4fec0b2594da2f94a4e34c088231831e91e1182
decktech <mdecker113@gmail.com>
2023-01-05 14:51:40 -0500

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
@@ -15,6 +16,19 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', goalRouter)
 app.use('/api/users', userRouter)
+
+// Serve frontend
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) => 
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  )
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'))
+}
 
 app.use(errorHandler)
 
